096a87b758bd3aa29f99d1775d188f016577fbaf
Prasanna R <82939539+prasannajitwta@users.noreply.github.com>
2021-05-22 11:02:06 +0530

    Add files via upload

---
+++ Program 2/Challenge2/pr.js
@@ -0,0 +1,5 @@
+for(var i=100;i<=1000;i++) 
+if(i%100==0) 
+          { 
+              document.write("<tr align='center'><td>" + i + "</td><td>" + i*i +" </td><td>" + i*i*i + "</td></tr>") 
+          } 
